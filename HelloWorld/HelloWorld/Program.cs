c758372fccddca64bc21bfbdeb4914886221f006
Bart <Bartdb2001@gmail.com>
2021-02-09 15:23:31 +0100

    first commit

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
